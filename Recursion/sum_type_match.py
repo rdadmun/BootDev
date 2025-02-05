4405ec6fe7f1f3b2e3f89c05e5c46f93ce337669
Ryan Dadmun <rdadmun@gmail.com>
2025-02-04 21:36:22 -0500

    Resolved merge conflicts

--- Recursion/sum_type_match.py
+++ Recursion/sum_type_match.py
@@ -24,3 +24,17 @@ def convert_format(content, from_format, to_format):
             return f"# {content.lstrip('<h1>').rstrip('</h1>')}"
     else:
         raise Exception('Invalid type')
+    
+    
+# OR
+
+def convert_format_2(content, from_format, to_format):
+    match (from_format, to_format):
+        case (DocFormat.MD, DocFormat.HTML):
+            return content.replace("# ", "<h1>") + "</h1>"
+        case (DocFormat.TXT, DocFormat.PDF):
+            return f"[PDF] {content} [PDF]"
+        case (DocFormat.HTML, DocFormat.MD):
+            return content.replace("<h1>", "# ").replace("</h1>", "")
+        case _:
+            raise Exception("Invalid type")
